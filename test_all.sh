76b4b5def978049dc87aa7bdef8a5ecbba62c161
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-06-03 06:45:02 +0200

    add script to make sure all scripts work

---
+++ test_all.sh
@@ -0,0 +1,16 @@
+python3 EM.py 
+python3 gini.py
+python3 classifiers.py
+python3 density_estimation.py
+python3 distance_functions.py
+python3 entropy.py
+python3 kmeans.py
+python3 knn.py
+python3 lof.py
+python3 lof_sklearn.py
+python3 n-dice.py
+python3 powerset.py
+python3 settheory.py
+python3 shared_nearest_neighbours.py
+python3 td2.py
+
